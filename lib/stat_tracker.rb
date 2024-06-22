6210ed80b97b218b0c178d367a7b0dc007e162fb
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2024-06-21 18:44:14 -0600

    game statistics

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -15,4 +15,69 @@ class StatTracker
     game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
     new(games, teams, game_teams)
   end
+
+  def total_goals_per_game
+    @games.map do |row|
+      away_goals = row[:away_goals].to_i
+      home_goals = row[:home_goals].to_i
+      total_goals = away_goals + home_goals
+      { game_id: row[:game_id], total_goals: total_goals }
+    end
+  end
+
+  def highest_total_score
+    highest_scoring_game = total_goals_per_game.max_by { |game| game[:total_goals] }
+    highest_scoring_game[:total_goals]
+  end
+
+  def lowest_total_score
+    lowest_scoring_game = total_goals_per_game.min_by { |game| game[:total_goals] }
+    lowest_scoring_game[:total_goals]
+  end
+
+  def total_games
+    @games.length
+  end
+
+  def number_of_home_wins
+    @games.count { |game| game[:home_goals].to_i > game[:away_goals].to_i }
+  end
+
+  def number_of_visitor_wins
+    @games.count { |game| game[:away_goals].to_i > game[:home_goals].to_i }
+  end
+
+  def number_of_ties
+    @games.count { |game| game[:away_goals].to_i == game[:home_goals].to_i }
+  end
+
+  def percentage_home_wins
+    (number_of_home_wins.to_f / total_games).round(2)
+  end
+
+  def percentage_visitor_wins
+    (number_of_visitor_wins.to_f / total_games).round(2)
+  end
+
+  def percentage_ties
+    (number_of_ties.to_f / total_games).round(2)
+  end
+
+  def count_of_games_by_season
+    @games.group_by { |game| game[:season] }.transform_values(&:count)
+  end
+
+  def average_goals_per_game
+    (total_goals_per_game.sum { |game| game[:total_goals] }.to_f / total_games).round(2)
+  end
+
+  def average_goals_by_season
+    seasons_hash = @games.group_by { |game| game[:season] }
+    seasons_hash.each do |season, season_games|
+      total_goals_per_season = season_games.sum do |game|
+        game[:away_goals].to_i + game[:home_goals].to_i
+      end
+      seasons_hash[season] = (total_goals_per_season.to_f / season_games.count).round(2)
+    end
+  end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,51 +1,99 @@
 require 'rspec'
 require 'spec_helper'
 require './lib/stat_tracker'
+require 'tempfile'
 
 RSpec.configure do |config|
   config.formatter = :documentation
 end
 
 RSpec.describe StatTracker do
-  it 'exists' do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+  before(:all) do
+    @games_file = Tempfile.new(['games', '.csv'])
+    @teams_file = Tempfile.new(['teams', '.csv'])
+    @game_teams_file = Tempfile.new(['game_teams', '.csv'])
+
+    CSV.open(@games_file.path, 'wb') do |csv|
+      csv << %w[game_id season type date_time away_team_id home_team_id away_goals home_goals venue venue_link]
+      csv << [1, 20122013, 'Postseason', '5/16/13', 3, 6, 2, 3, 'Toyota Stadium', '/api/v1/venues/null']
+      csv << [2, 20132014, 'Regular Season', '3/29/14', 18, 25, 3, 5, 'SeatGeek Stadium', '/api/v1/venues/null']
+      csv << [3, 20172018, 'Regular Season', '12/28/17', 17, 1, 3, 3, 'Mercedes-Benz Stadium', '/api/v1/venues/null']
+      csv << [4, 20182019, 'Postseason', '5/28/19', 6, 18, 1, 0, 'Heinz Field', '/api/v1/venues/null']
+    end
+
+    CSV.open(@teams_file.path, 'wb') do |csv|
+      csv << %w[team_id franchiseId teamName abbreviation Stadium link]
+      csv << []
+    end
+
+    CSV.open(@game_teams_file.path, 'wb') do |csv|
+      csv << %w[game_id team_id HoA result settled_in head_coach goals shots tackles pim powerPlayOpportunities powerPlayGoals faceOffWinPercentage giveaways takeaways]
+      csv << []
+    end
 
     locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
+      games: @games_file.path,
+      teams: @teams_file.path,
+      game_teams: @game_teams_file.path
     }
 
-    games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
-    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
-    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
 
-    stat_tracker = StatTracker.new(games, teams, game_teams)
-    expect(stat_tracker).to be_a(StatTracker)
+  after(:all) do
+    @games_file.close
+    @games_file.unlink
+    @teams_file.close
+    @teams_file.unlink
+    @game_teams_file.close
+    @game_teams_file.unlink
+  end
+
+  it 'exists' do
+    expect(@stat_tracker).to be_a(StatTracker)
   end
 
   it 'can read csv files from a locations hash' do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-
-    expect(stat_tracker.games).to be_a(CSV::Table)
-    expect(stat_tracker.teams).to be_a(CSV::Table)
-    expect(stat_tracker.game_teams).to be_a(CSV::Table)
+    expect(@stat_tracker.games).to be_a(CSV::Table)
+    expect(@stat_tracker.teams).to be_a(CSV::Table)
+    expect(@stat_tracker.game_teams).to be_a(CSV::Table)
   end
 
   describe 'Game Statistics' do
     it 'can calculate the highest total score' do
+      expect(@stat_tracker.highest_total_score).to eq(8)
+    end
+
+    it 'can calculate the lowest total score' do
+      expect(@stat_tracker.lowest_total_score).to eq(1)
+    end
+
+    it 'can calculate the percentage of home team wins' do
+      expect(@stat_tracker.percentage_home_wins).to eq(0.50)
+    end
+
+    it 'can calculate the percentage of visitor team wins' do
+      expect(@stat_tracker.percentage_visitor_wins).to eq(0.25)
+    end
+
+    it 'can calculate the percentage of ties' do
+      expect(@stat_tracker.percentage_ties).to eq(0.25)
+    end
+
+    it 'can count games by season' do
+      expected_hash = { "20122013" => 1, "20132014" => 1, "20172018" => 1, "20182019" => 1 }
+
+      expect(@stat_tracker.count_of_games_by_season).to eq(expected_hash)
+    end
+
+    it 'can count the average number of goals per game' do
+      expect(@stat_tracker.average_goals_per_game).to eq(5.0)
+    end
+
+    it 'can calculate the average number of goals by season' do
+      expected_hash = { "20122013" => 5, "20132014" => 8, "20172018" => 6, "20182019" => 1 }
+
+      expect(@stat_tracker.average_goals_by_season).to eq(expected_hash)
     end
   end
 end
